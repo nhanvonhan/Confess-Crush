302e0f441e30a4d4d3961e62558b44a698fde4c8
nhanvonhan <106798198+nhanvonhan@users.noreply.github.com>
2022-06-03 13:23:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi tình yêu to bự ",
-    introTitle: 'Hiếu Đỗ',
+    titleWeb: "Mini rúp ",
+    introTitle: 'Everyone',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
